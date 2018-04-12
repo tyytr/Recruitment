2da741e2a9354d43765d85674d7834d86c1f1f1b
gaoyp <gyp0925vip@outlook.com>
2018-04-12 11:18:35 +0800

    demo

--- src/main/java/com/recruitment/www/rest/UserController.java
+++ src/main/java/com/recruitment/www/rest/UserController.java
@@ -4,10 +4,8 @@ import com.recruitment.www.common.RestResp;
 import com.recruitment.www.entity.User;
 import com.recruitment.www.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @Author: Gaoyp
@@ -15,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @Date: Create in 下午3:22 2018/3/15
  * @Modified By:
  */
-@RestController
+@Controller
 @RequestMapping("/user")
 public class UserController {
 
@@ -23,12 +21,17 @@ public class UserController {
     private UserService userService;
 
 
+    @GetMapping("/index")
+    public String addUser(){
+        return "index";
+    }
+
     /**
      * 注册用户
      * @param user 用户
      */
     @PostMapping("/add")
-    public RestResp addUser(@RequestBody User user){
+    public @ResponseBody RestResp addUser(@RequestBody User user){
         return userService.addUser(user);
     }
 
